709d709d40723f6ff80e75dc712036f7f5837a3f
matthew.aedwards@gmail.com <matthew.aedwards@gmail.com>
2018-08-27 21:15:35 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow
-color= ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
-
-
-puts "color=,#{color[0]},#{color[1]},#{color[2]},#{color[3]},#{color[4]},
-#{color[5]},#{color[6]},#{color[7]}"
-
-
 
 end
